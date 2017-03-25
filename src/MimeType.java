b12efdc8fe2635bee8a1c1f4de7ae1ba6be85343
Marco M <gottacatchmall@gmail.com>
2017-03-25 21:11:10 +0000

    Sincronizzato il metodo getTypeFromFilename di MimeType, aggiunta variabile semaforo e documentazione

--- src/MimeType.java
+++ src/MimeType.java
@@ -12,10 +12,17 @@ import java.util.HashMap;
 public class MimeType
 {
 	private static HashMap<String, String> types;
-	private static boolean isInstanced;
+	private static boolean isInstanced = false;
 	private static final String UNKNOWN_TYPE_IDENTIFIER = "text/plain";
 	private static final String CONFIG_PATH = "MIME.conf";
 
+	// Il costruttore viene chiamato una sola volta, cioe' quando uno dei metodi viene chiamato per la prima volta.
+	// Leggendo il file indicato da CONFIG_PATH costrusce un dizionario che ha come chiavi le estensioni dei file e come
+	// valore il tipo MIME associato
+	// Il file e' cosi' costruito:
+	// <estensione> <mime-type>
+	// <estensione> <mime-type>
+	// ...
 	private MimeType()
 	{
 		try
@@ -32,6 +39,7 @@ public class MimeType
 				String[] token = line.split(" ");
 				types.put(token[0], token[1]);
 			}
+			isInstanced = true;
 		}
 		catch(IOException ie)
 		{
@@ -40,10 +48,18 @@ public class MimeType
 	}
 
 	// Dato il nome di un file viene ritornato il MIME Type corrispondente alla sua estensione
-	public static String getTypeFromFilename(String filename)
+	// Il metodo e' sincronizzato per evitare il caso che 2 entita' eseguono il controllo per verificare
+	// se la classe e' gia' stata instanziata in precedenza allo stesso tempo, generando un problema di concorrenza.
+	// Sebbene l'unico effetto negativo sarebbe che le risorse verrebbero' caricate 2 volte, rendendo il metodo sincronizzato
+	// si elimina il problema alla radice.
+	public synchronized static String getTypeFromFilename(String filename)
 	{
-		if(types == null)
+		// Si controlla che la classe sia stata instanziata precedentemente, che significherebbe che tutte le risorse sono gia' state ricaricate.
+		// Se non lo e' viene instanziata e le risorse caricate.
+		if(!isInstanced)
 			new MimeType();
+		// Per poter ricavare l'estensione dal nome del file si cerca l'indice dell'ultimo punto all'interno della stringa,
+		// dopodiche' si estrapola una sottostringa che inizia dall'indice trovato (+ 1 per saltare il punto) alla fine della stringa
 		int lastDotIndex = filename.indexOf(".");
 		if(lastDotIndex < 0)
 			return UNKNOWN_TYPE_IDENTIFIER;
